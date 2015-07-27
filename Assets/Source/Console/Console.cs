f5e388e175e2eb4694a7e5ae6566339b8e46dd90
nunomota <hypernuno@hotmail.com>
2015-07-27 20:45:41 +0100

    Adds .gitignore and base files for project

---
+++ Assets/Source/Console/Console.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Console {
+
+	private static int maxMessages = 20;	//maximum number of lines the console will keep stored
+	private string[] msgList;				//list of stored messages
+	private int indexOfLast;				//index of last string inserted (needed before reaching 'maxMessages')
+	private Vector2 dimensions;				//dimensions of the console window
+
+	//class' constructor
+	public Console(Vector2 dimensions) {
+		this.dimensions = dimensions;
+		msgList = new string[maxMessages];
+		indexOfLast = 0;
+	}
+
+	//appends text at the end of current line
+	public void write(string msg) {
+
+	}
+
+	//adds a new line to the console
+	public void writeLine(string msg) {
+
+	}
+}
