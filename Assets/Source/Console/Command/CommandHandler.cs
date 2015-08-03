17183c2e297814767523435341db9fbfe5205417
nunomota <hypernuno@hotmail.com>
2015-08-04 00:10:47 +0100

    Implements simple usage of 'help' command using new prints and HashTable

--- Assets/Source/Console/Command/CommandHandler.cs
+++ Assets/Source/Console/Command/CommandHandler.cs
@@ -8,14 +8,15 @@ public class CommandHandler {
 
 	private TextAsset commandDB;				//the XML that will be read
 	private MessageType commandMessageType;		//the custom MessageType for console prints
-	private List<CommandInfo> commandList;		//the list of supported commands
+	private List<CommandInfo> commandList;		//the list of supported commands (to be iterated)
+	private HashTable commandTable;				//hashtable of supportted commands (for quick search)
 
 	public CommandHandler() {
 		this.commandDB = Resources.Load ("Console/Commands/DataBase") as TextAsset;
 		this.commandMessageType = new MessageType("[CmdHndl]");
 		this.commandList = new List<CommandInfo>();
 
-		readXml();
+		loadXml();
 	}
 
 	//values below 0 mean an error ocurred
@@ -28,6 +29,7 @@ public class CommandHandler {
 			string[] options = command.getOptions();
 			if (options.Length == 0) {
 				//TODO print the entire list of commands
+				printCommandList();
 			} else {
 				//TODO print only the command's information
 			}
@@ -39,7 +41,7 @@ public class CommandHandler {
 		return 0;
 	}
 
-	private void readXml() {
+	private void loadXml() {
 		XmlDocument xmlDoc = new XmlDocument();
 		xmlDoc.LoadXml(commandDB.text);
 		XmlNodeList commandTempList = xmlDoc.GetElementsByTagName("Command");
@@ -68,6 +70,24 @@ public class CommandHandler {
 				}
 			}
 			commandList.Add(newCommandInfo);
+		}
+		createHashTable();
+	}
+
+	private void createHashTable() {
+		commandTable = new HashTable(commandList.Count);
+		foreach(CommandInfo commandInfo in commandList) {
+			commandTable.add(commandInfo);
+		}
+	}
+
+	private void printCommandList() {
+		CommandInfo helpInfo = commandTable.get("help");
+		RIPBugs.console.writeLine(helpInfo.getDescription());
+		foreach(CommandInfo commandInfo in commandList) {
+			if (commandInfo.getName() != "help") {
+				RIPBugs.console.writeLine(commandInfo.simpleString(), commandMessageType, 0);
+			}
 		}
 	}
 }
