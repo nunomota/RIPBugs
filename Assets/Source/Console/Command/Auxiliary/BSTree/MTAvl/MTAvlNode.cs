6f780d6ab0a4ec9271a3db8ac878c01904c8e697
nunomota <hypernuno@hotmail.com>
2015-08-10 19:53:52 +0100

    Starts implementing an AVL tree (for optimized MessageType search) and Changes CommandHashTable directory

---
+++ Assets/Source/Console/Command/Auxiliary/BSTree/MTAvl/MTAvlNode.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// MTAvlTree node.
+/// </summary>
+public class MTAvlNode {
+
+	private int weight;
+	private MTAvlNode up, left, right;
+	private MessageType value;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MTAvlNode"/> class.
+	/// </summary>
+	/// <param name="value">MessageType that will define the node.</param>
+	public MTAvlNode(MessageType value) {
+		this.weight = 0;
+		this.up = null;
+		this.left = null;
+		this.right = null;
+		this.value = value;
+	}
+
+	/// <summary>
+	/// Compare the specified node1 and node2.
+	/// </summary>
+	/// <returns>
+	/// Ultimately will return the result of string.Compare(tag1, tag2), where tag represents the same as node.getValue().getTag()
+	/// </returns>
+	/// <param name="node1">First <see cref="MTAvlNode"/>.</param>
+	/// <param name="node2">Second <see cref="MTAvlNode"/>.</param>
+	public static int Compare(MTAvlNode node1, MTAvlNode node2) {
+		return node1.CompareTo(node2);
+	}
+
+	/// <summary>
+	/// Compares the node itself with a specified one.
+	/// </summary>
+	/// <returns>
+	/// Will return the result of string.Compare(tag1, tag2), where tag represents the same as node.getValue().getTag()
+	/// </returns>
+	/// <param name="cmpNode">Target <see cref="MTAvlNode"/>.</param>
+	public int CompareTo(MTAvlNode cmpNode) {
+		return string.Compare(this.getValue().getTag(), cmpNode.getValue().getTag());
+	}
+
+	/* -------------------------------
+	 * ------ Getters & Setters ------
+	 ------------------------------- */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public void setUp(MTAvlNode up) {
+		this.up = up;
+	}
+
+	public void setLeft (MTAvlNode left) {
+		this.left = left;
+	}
+
+	public void setRight (MTAvlNode right) {
+		this.right = right;
+	}
+
+	public void setValue (MessageType value) {
+		this.value = value;
+	}
+
+	public int getWeight() {
+		return this.weight;
+	}
+
+	public MTAvlNode getUp() {
+		return this.up;
+	}
+
+	public MTAvlNode getLeft() {
+		return this.left;
+	}
+
+	public MTAvlNode getRight() {
+		return this.right;
+	}
+
+	public MessageType getValue() {
+		return this.value;
+	}
+}
---
+++ Assets/Source/Console/Command/Auxiliary/BSTree/MTAvl/MTAvlTree.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class that represents a custom made AVL tree. 
+/// Aims to optimize the search of MessageType objects only.
+/// </summary>
+public class MTAvlTree {
+
+	private MTAvlNode root;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MTAvlTree"/> class.
+	/// </summary>
+	public MTAvlTree () {
+		this.root = null;
+	}
+	
+	/// <summary>
+	/// Method called from outside the class to add a new MessageType to the tree
+	/// </summary>
+	/// <param name="value">MessageType to add.</param>
+	public void add(MessageType value) {
+		MTAvlNode curNode = null;
+		MTAvlNode nextNode = root;
+		MTAvlNode newNode = new MTAvlNode(value);
+		int nodeCmp = 0;							//it is irrelevant the initial value set to this variable
+		while (nextNode != null) {
+			curNode = nextNode;
+			if ((nodeCmp = newNode.CompareTo(curNode)) != 0) {
+				nextNode = (nodeCmp < 0)? curNode.getLeft(): curNode.getRight();
+			} else {
+				//another MessageType already exists with the exact same tag... don't add a new one
+				//TODO add some kind of warning
+				return;
+			}
+		}
+		insertNode(curNode, newNode, nodeCmp);
+	}
+
+	/// <summary>
+	/// Inserts a MTAvlNode as a child of another MTAvlNode
+	/// </summary>
+	/// <param name="father">Node to be used as a 'father'.</param>
+	/// <param name="newNode">Node to be added as a 'child'.</param>
+	/// <param name="nodeCmp">Value that indicates the side in which to add the 'child' (right or left).</param>
+	private void insertNode(MTAvlNode father, MTAvlNode newNode, int nodeCmp) {
+		if (father != null) {
+			newNode.setUp(father);
+			if (nodeCmp < 0) {
+				father.setLeft(newNode);
+			} else {
+				father.setRight(newNode);
+			}
+			balanceTree(newNode);
+		} else {
+			//means there was still no Node inserted in the tree
+			this.root = newNode;
+		}
+	}
+	
+	/// <summary>
+	/// Balances the AVL tree after inserting a node
+	/// </summary>
+	/// <param name="bottomNode">Bottom node.</param>
+	private void balanceTree(MTAvlNode bottomNode) {
+		//TODO balance the AVL tree
+	}
+}
